c0aea3cbe6057a7ab49d59fea7bf2087378b5ef8
lijingjiang <lijingjiangmason@gmail.com>
2016-10-29 16:32:35 -0500

    merged

--- app/src/main/java/com/example/lijingjiang/mobile_sensor_display/SimplePedometerActivity.java
+++ app/src/main/java/com/example/lijingjiang/mobile_sensor_display/SimplePedometerActivity.java
@@ -16,15 +16,23 @@
 
 package com.example.lijingjiang.mobile_sensor_display;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.apps.simplepedometer.R;
@@ -48,6 +56,28 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
      */
     private GoogleApiClient client;
 
+    /**
+     * The following part is intented for distance sensor logic
+     */
+    private TextView resultView;
+
+    /**
+     * we use the gps location to measure the distance of user travelled
+     */
+    private LocationManager locationManager;
+    private Location startLocation = null;
+    private Location lastKnown = null;
+    private LocationListener locationListener;
+
+    /**
+     * Location Provider, must set it as the network so the result is more precise
+     */
+    private String locationProvider;
+    private boolean firstLocationFlag = false;
+    private long minTimeMs = 100;
+    private long minDistanceMeters = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,6 +92,95 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        // Attach UI Objects
+        resultView = (TextView) findViewById(R.id.result_field);
+
+
+
+        // Attach Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        if (!canAccessLocation()) {
+            requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
+        }
+
+        // Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                float[] results = new float[3];
+                if(lastKnown != null && startLocation != null) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+
+                    // Stop periodic location updates
+                    locationManager.removeUpdates(locationListener);
+
+                    // Calculate distance between startLocation and lastKnown and store in results[]
+                    Location.distanceBetween(startLocation.getLatitude(), startLocation.getLongitude(),
+                            lastKnown.getLatitude(), lastKnown.getLongitude(), results);
+                }
+
+                // display results
+                resultView.setText((String.valueOf(results[0])));
+
+                if (isBetterLocation(location, lastKnown)) {
+
+                    if (firstLocationFlag) {
+                        startLocation = location;
+                        firstLocationFlag = false;
+                    }
+                    lastKnown = location;
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        /**
+         * Handle with newest API 24 permission requirement system
+         */
+
+        // Start Retrieving Location Updates from NETWORK_PROVIDER
+        if (ActivityCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTimeMs, minDistanceMeters, locationListener);
+
+        /**
+         * Choose the network listener on default
+         */
+        // setup UI
+        resultView.setText(R.string.zero);
+
+        // Set up location management for NETWORK_PROVIDER
+        locationProvider = LocationManager.NETWORK_PROVIDER;
+        lastKnown = locationManager.getLastKnownLocation(locationProvider);
+
+        // Start Retrieving Location Updates from NETWORK_PROVIDER
+        if (ActivityCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTimeMs, minDistanceMeters, locationListener);
+
     }
 
     @Override
@@ -134,6 +253,15 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
 
     // resets and collects new data.
     public void startCollection(View view){
+        // set flag to tell onLocationChanged() to
+        // save its last location as the startLocation
+        // handle null pointer exception when the app is setup the gps location in the backend
+        firstLocationFlag = true;
+
+
+        // every time start, you should restart it
+        resultView.setText(R.string.zero);
+
         numSteps = 0;
         textView.setText(TEXT_NUM_STEPS + numSteps);
         sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
@@ -141,9 +269,110 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
 
     // pass collected data to result activity for analysis
     public void stopCollection(View view){
+        /**
+         * The following logic is used to process the distance display logic
+         */
+
+        float[] results = new float[3];
+        if(lastKnown != null && startLocation != null) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+
+            // Stop periodic location updates
+            locationManager.removeUpdates(locationListener);
+
+            // Calculate distance between startLocation and lastKnown and store in results[]
+            Location.distanceBetween(startLocation.getLatitude(), startLocation.getLongitude(),
+                    lastKnown.getLatitude(), lastKnown.getLongitude(), results);
+        }
+
+        // display results
+        resultView.setText((String.valueOf(results[0])));
+
+
         sensorManager.unregisterListener(this);
         Intent intent = new Intent(this, ResultsActivity.class);
         intent.putExtra("num_steps",numSteps);
         startActivity(intent);
     }
+
+
+    /**
+     * The following are the functions we need to get precise distance measurement
+     */
+
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
+    /**
+     * Helper function for permission check
+     */
+
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+    private static final String[] INITIAL_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_CONTACTS
+    };
+    private static final int INITIAL_REQUEST=1337;
 }
