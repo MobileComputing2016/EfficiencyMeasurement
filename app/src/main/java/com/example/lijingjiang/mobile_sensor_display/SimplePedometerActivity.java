2c3c343661523d7e83b46522f6854eb81d787aff
lijingjiang <lijingjiangmason@gmail.com>
2016-10-31 11:41:21 -0500

    commit with google service api

--- app/src/main/java/com/example/lijingjiang/mobile_sensor_display/SimplePedometerActivity.java
+++ app/src/main/java/com/example/lijingjiang/mobile_sensor_display/SimplePedometerActivity.java
@@ -20,6 +20,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -30,10 +31,14 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.apps.simplepedometer.R;
 import com.google.android.gms.appindexing.Action;
@@ -41,11 +46,20 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+//import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+
 import java.sql.SQLOutput;
 
 import static com.google.android.apps.simplepedometer.R.layout.activity_results;
 
-public class SimplePedometerActivity extends Activity implements SensorEventListener, StepListener {
+public class SimplePedometerActivity extends Activity implements SensorEventListener, StepListener, ConnectionCallbacks, OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
     private TextView textView;
     private SimpleStepDetector simpleStepDetector;
     private SensorManager sensorManager;
@@ -88,6 +102,16 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
     private float minDistanceMeters = 0.1f;
     private double accumulatedDistance = 0.0;
 
+
+    /**
+     * To access google api
+     */
+    //Define a request code to send to Google Play services
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double currentLatitude;
+    private double currentLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -192,6 +216,22 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
         // change the following gps provider when we get gps
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTimeMs, minDistanceMeters, locationListener);
 
+        /**
+         * Initialize the field required by google service
+         */
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(100)        // 100 milliseconds
+                .setFastestInterval(19); // 10 milliseconds
     }
 
     @Override
@@ -200,11 +240,26 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
         numSteps = 0;
         textView.setText(TEXT_NUM_STEPS + numSteps);
 
+        /**
+         * Required by google api
+         */
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onPause() {
         super.onPause();
+
+        /**
+         * Required by google api
+         */
+        Log.v(this.getClass().getSimpleName(), "onPause()");
+
+        //Disconnect from API onPause()
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
 
     }
 
@@ -399,4 +454,62 @@ public class SimplePedometerActivity extends Activity implements SensorEventList
             Manifest.permission.READ_CONTACTS
     };
     private static final int INITIAL_REQUEST=1337;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+            //If everything went fine lets get latitude and longitude
+            currentLatitude = location.getLatitude();
+            currentLongitude = location.getLongitude();
+
+            Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                    /*
+                     * Thrown if Google Play services canceled the original
+                     * PendingIntent
+                     */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+                /*
+                 * If no resolution is available, display a dialog to the
+                 * user with the error.
+                 */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+    }
 }
